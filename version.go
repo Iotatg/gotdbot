2f249b2f695cfa584e4b1308600f55208298acc0
Iota coder <Iotatg@users.noreply.github.com>
2026-02-25 17:14:08 +0530

    Update version to v0.5.0

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package gotdbot
 
 // Version is the current version of the library.
-const Version = "v0.4.0"
+const Version = "v0.5.0"
 
 // TDLibVersion is the version of the TDLib used by the library.
 const TDLibVersion = "v1.8.61"
